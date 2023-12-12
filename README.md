c53d6c8c35ec582bea6d4fc8b7a9362e88326e14
vd <raphaelvorias@gmail.com>
2023-12-12 14:38:02 +0100

    add consistency decoder

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# glif-comfy
-custom plugins for comfyUI
+# ComfyUI-GlifNodes
+
+Custom nodes for comfyUI.
+
+# Install
+
+```
+pip install -r requirements.txt
+```
+
+# Nodes
+
+<details>
+  <summary><b><code>GlifConsistencyVAE</code></b> openai's consistency decoder from hf hub</summary>
+    <img src="docs/consistency_decoder.png" height="500px"/>
+</details>
+
+<br>
+
+# Acknowledgements
+
+diffusers: https://github.com/huggingface/diffusers
+openai - consistencydecoder: https://github.com/openai/consistencydecoder
---
+++ nodes.py
@@ -0,0 +1,50 @@
+import os
+import torch
+from diffusers import ConsistencyDecoderVAE
+
+
+def find_and_maybe_create_cache():
+    cwd = os.getcwd()
+    if os.path.exists(os.path.join(cwd, "ComfyUI")):
+        cwd = os.path.join(cwd, "ComfyUI")
+    if os.path.exists(os.path.join(cwd, "models")):
+        cwd = os.path.join(cwd, "models")
+    if not os.path.exists(os.path.join(cwd, "huggingface_cache")):
+        print("Creating huggingface_cache directory within comfy")
+        os.mkdir(os.path.join(cwd, "huggingface_cache"))
+    
+    return str(os.path.join(cwd, "huggingface_cache"))
+
+
+class ConsistencyDecoder:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {"latent": ("LATENT",)}}
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "decode"
+    CATEGORY = "latent"
+
+    def __init__(self):
+        self.vae = ConsistencyDecoderVAE.from_pretrained(
+            "openai/consistency-decoder",
+            torch_dtype=torch.float16,
+            variant="fp16",
+            use_safetensors=True,
+            cache_dir=find_and_maybe_create_cache(),
+        ).to("cuda")
+        
+    def decode(self, latent):    
+        print(latent["samples"].shape)
+        sample = self.vae.decode(latent["samples"].half().cuda()).sample
+        sample = sample.clamp(-1, 1).movedim(1,-1).add(1.).mul(0.5)
+        return (sample, )
+
+
+NODE_CLASS_MAPPINGS = {
+    "GlifConsistencyVAE": ConsistencyDecoder,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "GlifConsistencyVAE": "Consistency VAE Decoder",
+}
