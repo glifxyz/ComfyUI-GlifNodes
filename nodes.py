f0a9ebe861f2acb2819959b4c39a965b1e3ffb7d
vd <raphaelvorias@gmail.com>
2023-12-14 12:46:32 +0100

    breaking: add patcher

--- nodes.py
+++ nodes.py
@@ -1,9 +1,10 @@
 import os
 import torch
+from comfy import model_management
 from diffusers import ConsistencyDecoderVAE
 
 
-def find_and_maybe_create_cache():
+def find_or_create_cache():
     cwd = os.getcwd()
     if os.path.exists(os.path.join(cwd, "ComfyUI")):
         cwd = os.path.join(cwd, "ComfyUI")
@@ -31,20 +32,49 @@ class ConsistencyDecoder:
             torch_dtype=torch.float16,
             variant="fp16",
             use_safetensors=True,
-            cache_dir=find_and_maybe_create_cache(),
-        ).to("cuda")
+            cache_dir=find_or_create_cache(),
+        ).eval().to("cuda")
+
+    def _decode(self, latent):
+        """Used when patching another vae."""
+        return self.vae.decode(latent.half().cuda()).sample
         
-    def decode(self, latent):    
-        print(latent["samples"].shape)
-        sample = self.vae.decode(latent["samples"].half().cuda()).sample
+    def decode(self, latent):
+        """Used for standalone decoding."""
+        sample = self._decode(latent["samples"])
         sample = sample.clamp(-1, 1).movedim(1,-1).add(1.).mul(0.5).cpu()
         return (sample, )
+    
+class PatchDecoderTiled:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {"vae": ("VAE",)}}
+    
+    RETURN_TYPES = ("VAE",)
+    FUNCTION = "patch"
+    category = "vae"
+
+    def __init__(self):
+        self.vae = ConsistencyDecoder()
+
+    def patch(self, vae):
+        del vae.first_stage_model.decoder
+        vae.first_stage_model.decode = self.vae._decode
+        vae.decode = lambda x: vae.decode_tiled_(
+            x,
+            tile_x=512, tile_y=512,
+            overlap=64,
+        ).to("cuda").movedim(1,-1)
+
+        return (vae,)
 
 
 NODE_CLASS_MAPPINGS = {
-    "GlifConsistencyVAE": ConsistencyDecoder,
+    "GlifConsistencyDecoder": ConsistencyDecoder,
+    "GlifPatchConsistencyDecoderTiled": PatchDecoderTiled,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "GlifConsistencyVAE": "Consistency VAE Decoder",
+    "GlifConsistencyDecoder": "Consistency VAE Decoder",
+    "GlifPatchConsistencyDecoderTiled": "Patch Consistency VAE Decoder",
 }
